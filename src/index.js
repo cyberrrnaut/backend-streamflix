33dbc78bacb6716698ec7fd1dae4f6b5d50ef657
Soumyendu <cyberrrnaut@gmail.com>
2024-01-07 01:43:17 +0530

    setting up api error and response

--- src/index.js
+++ src/index.js
@@ -5,4 +5,12 @@ dotenv.config({
     path: './env'
 })
 
-connectDB()
+connectDB()             //async function returns a promise
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port  ${process.env.PORT }`);
+    })
+})
+.catch((error)=>{
+    console.log("Database connection error:",error);
+}) 
